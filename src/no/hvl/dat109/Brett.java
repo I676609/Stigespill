c916a3b182b2f121b0a4a82f10e86fbd8bcdc268
I676609 <676609@stud.hvl.no>
2025-01-27 13:48:13 +0100

    switch ferdig

--- src/no/hvl/dat109/Brett.java
+++ src/no/hvl/dat109/Brett.java
@@ -44,7 +44,28 @@ public class Brett {
 		case 100:
 		case 80:
 			System.out.println("Spiller : " + spiller.getFarge() + "har kommet i mål!");
-
+			break;
+			
+		case 98: spiller.setPlassering(78);break;
+		case 95: spiller.setPlassering(75); break;
+		case 92: spiller.setPlassering(73); break;
+		case 87: spiller.setPlassering(24); break;
+		case 71: spiller.setPlassering(91);break;
+		case 64: spiller.setPlassering(60);break;
+		case 62: spiller.setPlassering(18);break;
+		case 56: spiller.setPlassering(53);break;
+		case 51: spiller.setPlassering(67);break;
+		case 49: spiller.setPlassering(11);break;
+		case 47: spiller.setPlassering(11);break;
+		case 36: spiller.setPlassering(44);break;
+		case 28: spiller.setPlassering(84);break;
+		case 21: spiller.setPlassering(42);break;
+		case 16: spiller.setPlassering(6);break;
+		case 6: spiller.setPlassering(31);break;
+		case 4: spiller.setPlassering(14);break;
+		case 2: spiller.setPlassering(38);break;
+		default: return;
+		
 		}
 
 	}
