936efe52e2898ef1b3ae860951e64744258b59d9
h668805 <668805@stud.hvl.no>
2025-01-29 12:11:40 +0100

    l

--- src/no/hvl/dat109/test/TestSpill.java
+++
@@ -1,5 +0,0 @@
-package no.hvl.dat109.test;
-
-public class TestSpill {
-
-}
--- src/no/hvl/dat109/test/TestSpiller.java
+++ src/no/hvl/dat109/test/TestSpiller.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import no.hvl.dat109.Farge;
+import no.hvl.dat109.Spiller;
+
 class TestSpiller {
+	private Spiller spiller = new Spiller(Farge.BLÅ);
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTrillTerning() {
+		int tall = spiller.trillTerning();
+		assertTrue(tall >= 1);
+		assertTrue(tall <= 6);
 	}
 
 }
